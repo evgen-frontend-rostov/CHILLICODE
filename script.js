bc703bd971af911df51f0be503397a2d9b09e968
evgen-frontend-rostov <googlbond1991@gmail.com>
2021-11-17 11:06:08 +0300

    feat: added function showAllPosts

---
+++ script.js
@@ -0,0 +1,24 @@
+// Показать посты по нажатию кнопки Show all posts
+
+let showAllPosts = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(allPosts => {
+            let wallPost = document.querySelector('.wall_item')
+            let htmlPost = allPosts.map(post => {
+                return (
+                    `<div class="wall_item_post">
+                         <p> ${post.userId} </p>
+                         <p> ${post.title} </p>
+                         <p> ${post.body} </p>
+                         <button class="btn_favorites">Add to favorites</button>
+                     </div>
+`)
+            })
+            wallPost.insertAdjacentHTML('afterbegin', htmlPost.join(' '))
+        });
+}
+
+let button2 = document.getElementById('btn_show')
+button2.addEventListener('click', showAllPosts)
+
